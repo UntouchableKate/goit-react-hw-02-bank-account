dde86c65b20910a72072105e06677695ab4285d3
Your Name <kateryna.skliaruk6@gmail.com>
2019-11-20 14:07:11 +0100

    added src

---
+++ src/components/Dashboard/Dashboard.js
@@ -0,0 +1,99 @@
+// Core
+import React, { Component } from 'react';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+import shortid from 'shortid';
+
+// Components
+import Controls from '../Controls';
+import Balance from '../Balance';
+import TransactionHistory from '../TransactionHistory';
+
+class Dashboard extends Component {
+  state = {
+    transactions: [],
+    balance: 0,
+  };
+
+  handleDeposit = amount => {
+    const newStateTransaction = {
+      id: shortid.generate(),
+      type: 'deposit',
+      amount: Number(amount),
+      date: new Date().toLocaleString(),
+    };
+    if (Number(amount) <= 0 || amount === null) {
+      toast.warn('Введите сумму для проведения операции!', {
+        position: toast.POSITION.BOTTOM_RIGHT,
+      });
+    } else {
+      this.setState(state => ({
+        transactions: [...state.transactions, newStateTransaction],
+        balance: Number(state.balance) + Number(amount),
+        // balance: state.balance + amount,
+      }));
+      toast.success('Транзакция была успешно проведена!', {
+        position: toast.POSITION.BOTTOM_RIGHT,
+      });
+    }
+  };
+
+  handleWithdraw = amount => {
+    const newStateTransaction = {
+      id: shortid.generate(),
+      type: 'withdraw',
+      amount: Number(amount),
+      date: new Date().toLocaleString(),
+    };
+    if (Number(amount) > this.state.balance) {
+      toast.error('На счету недостаточно средств для проведения операции!', {
+        position: toast.POSITION.BOTTOM_RIGHT,
+      });
+    } else if (amount <= 0 || amount === null) {
+      toast.warn('Введите сумму для проведения операции!', {
+        position: toast.POSITION.BOTTOM_RIGHT,
+      });
+    } else {
+      this.setState(state => ({
+        transactions: [...state.transactions, newStateTransaction],
+        balance: Number(state.balance) - Number(amount),
+        // balance: state.balance - amount,
+      }));
+      toast.success('Транзакция была успешно проведена!', {
+        position: toast.POSITION.BOTTOM_RIGHT,
+      });
+    }
+  };
+
+  handleSumTransaction = type => {
+    return this.state.transactions.reduce(
+      (acc, transaction) => {
+        return {
+          ...acc,
+          [transaction.type]: acc[transaction.type] + transaction.amount,
+        };
+      },
+      { deposit: 0, withdraw: 0 },
+    );
+  };
+
+  render() {
+    const { balance, transactions } = this.state;
+    const { deposit, withdraw } = this.handleSumTransaction();
+
+    return (
+      <div>
+        <Controls
+          onDeposit={this.handleDeposit}
+          onWithdraw={this.handleWithdraw}
+        />
+        <Balance balance={balance} income={deposit} expenses={withdraw} />
+        <TransactionHistory items={transactions} />
+        <ToastContainer />
+      </div>
+    );
+  }
+}
+
+export default Dashboard;
